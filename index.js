3d5abf313739e94f4eebd3597d2bbcfd113c1d60
Abhishek Sharma <hello.abhishek@outlook.com>
2021-02-06 01:27:02 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let loadMatch = function () {
+    var matches = [];
+    fetch('mainData.json')
+        .then(response => response.json())
+        .then(data => {
+            //load JSON data to a new variable
+            matches = data[1].data
+
+            //load random match
+            let randomMatch = matches[Math.floor(Math.random() * matches.length)];
+
+            //update the frontend with random match's data
+            let matchTitle = document.getElementById('matchTitle')
+            let matchLink = document.getElementById('matchLink')
+
+            matchTitle.innerHTML = randomMatch.title;
+            matchLink.innerHTML = randomMatch.url
+            matchLink.href = randomMatch.url
+        });
+}
+
+loadMatch();
+
+document.getElementById('reloadBtn').addEventListener('click', function(){
+    loadMatch();
+})
